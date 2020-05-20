dbfbeec9c01201d9ddfc43f7c8c2756ca5131388
yu-zyu <siragaigarasi@gmail.com>
2020-05-20 09:01:35 +0900

    fix 名前空間を追加

--- Assets/4_Factory_Method/Main.cs
+++ Assets/4_Factory_Method/Main.cs
@@ -2,16 +2,20 @@
 using FactoryMethodPattern_framework;
 using FactoryMethodPattern_idcard;
 
-public class Main : MonoBehaviour
+
+namespace FactoryMethodPattern
 {
-    void Start()
+    public class Main : MonoBehaviour
     {
-        Factory factory = new IDCardFactory();
-        Product card1 = factory.create("結城浩");
-        Product card2 = factory.create("とむら");
-        Product card3 = factory.create("佐藤花子");
-        card1.Use();
-        card2.Use();
-        card3.Use();
+        void Start()
+        {
+            Factory factory = new IDCardFactory();
+            Product card1 = factory.create("結城浩");
+            Product card2 = factory.create("とむら");
+            Product card3 = factory.create("佐藤花子");
+            card1.Use();
+            card2.Use();
+            card3.Use();
+        }
     }
 }
