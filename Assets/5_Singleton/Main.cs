c9a3eb684e8ff6b302dbdaa673859b93403ebd95
yu-zyu <siragaigarasi@gmail.com>
2020-05-20 09:20:21 +0900

    Singletonパターンを作った

---
+++ Assets/5_Singleton/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SingletonPattern
+{
+    public class Main : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Start");
+            Singleton obj1 = Singleton.getInstance();
+            Singleton obj2 = Singleton.getInstance();
+           if(obj1 == obj2)
+            {
+                Debug.Log("*obj1とobj2は同じインスタンスです。*");
+            }
+            else
+            {
+                Debug.Log("*obj1とobj2は同じインスタンスではありません。*");
+            }
+            Debug.Log("*End.*");
+        }
+    }
+}
---
+++ Assets/5_Singleton/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Debug.Log("インスタンスを生成しました。");
+        }
+        
+        public static Singleton getInstance()
+        {
+            return singleton;
+        }
+    }
+}
