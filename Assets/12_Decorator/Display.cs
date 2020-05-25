a8d9a74ea204171c9e2e458d1479345091525803
yu-zyu <siragaigarasi@gmail.com>
2020-05-25 09:26:50 +0900

    Decoratorパターンを作った

---
+++ Assets/12_Decorator/Display.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DecoratorPattern
+{
+    public abstract class Display
+    {
+        public abstract int GetColumns();
+        public abstract int GetRows();
+        public abstract string GetRowText(int row);
+        public void show()
+        {
+            for(int i = 0; i < GetRows(); i++)
+            {
+                Debug.Log(GetRowText(i));
+            }
+        }
+    }
+}
---
+++ Assets/12_Decorator/Main.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DecoratorPattern
+{
+    public class Main : MonoBehaviour
+    {
+        void Start()
+        {
+            Display b1 = new StringDisplay("Hello, world.");
+            Display b2 = new SideBorder(b1, '#');
+            Display b3 = new FullBorder(b2);
+            b1.show();
+            b2.show();
+            b3.show();
+            Display b4 =
+                new SideBorder(
+                    new FullBorder(
+                        new FullBorder(
+                            new SideBorder(
+                                new FullBorder(
+                                    new StringDisplay("こんにちは。")
+                                    ),
+                                '*'
+                              )
+                           )
+                        ),
+                        '/'
+                );
+            b4.show();
+        }
+    }
+}
