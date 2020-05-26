8c9614b44b10cb1aa7bc0b03673e008fdee32fc2
yu-zyu <siragaigarasi@gmail.com>
2020-05-26 21:48:14 +0900

    Mementoパターンを作った

---
+++ Assets/18_Memento/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using MementoPattern_game;
+
+namespace MementoPattern
+{
+    public class Main : MonoBehaviour
+    {
+        private void Start()
+        {
+            Gamer gamer = new Gamer(100);
+            Memento memento = gamer.createMemento();
+            for (int i = 0; i < 100; i++) 
+            {
+                Debug.Log("====" + i);
+                Debug.Log("現状:" + gamer);
+
+                gamer.bet();
+
+                Debug.Log("所持金は" + gamer.getMoney() + "円になりました。");
+
+                if(gamer.getMoney() > memento.getMoney())
+                {
+                    Debug.Log("    (だいぶ増えたので、現在の状態を保存しておこう)");
+                    memento = gamer.createMemento();
+                }else if(gamer.getMoney() < memento.getMoney() / 2)
+                {
+                    Debug.Log("    (だいぶ減ったので、以前の状態に復帰しよう) ");
+                    gamer.restoreMemento(memento);
+                }
+
+                try
+                {
+                    Thread.Sleep(1000);
+                }catch(Exception e)
+                {
+
+                }
+                Debug.Log("");
+            }
+        }
+    }
+}
---
+++ Assets/18_Memento/Memento.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MementoPattern_game
+{
+    public class Memento
+    {
+        int money;
+        ArrayList fruits;
+
+        public int getMoney()
+        {
+            return money;
+        }
+
+        public Memento(int money)
+        {
+            this.money = money;
+            this.fruits = new ArrayList();
+        }
+
+        public void addFruit(string fruit)
+        {
+            fruits.Add(fruit);
+        }
+
+        public ArrayList getFruits()
+        {
+            return (ArrayList)fruits.Clone();
+        }
+    }
+}
